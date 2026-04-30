4d3ea891ef10d33f7daa649bf461e480d66e4599
jae <jgangemi@gmail.com>
2026-04-30 12:28:58 -0600

    Revert "feat: share single SPI buffer for TX/RX (saves ~131 B RAM)"
    This reverts commit 8d152a391fd87ea6e5edf6d6114f97aa56a2781f.

--- src/ccspi.h
+++ src/ccspi.h
@@ -64,10 +64,7 @@
 typedef void (*gcSpiHandleRx)(void *p);
 typedef void (*gcSpiHandleTx)(void);
 
-extern char spi_buffer[];
-
-// wlan_tx_buffer is a macro alias to spi_buffer (shared half-duplex TX/RX buffer)
-#define wlan_tx_buffer ((unsigned char *)spi_buffer)
+extern unsigned char wlan_tx_buffer[];
 
 //*****************************************************************************
 //
